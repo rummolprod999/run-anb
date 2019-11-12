70a680214087f2ffa53cc1c5d0036259ee640d90
rummolprod999 <rummolprod999@gmail.com>
2019-11-12 15:37:25 +0300

    upd

---
+++ run_anb.py
@@ -0,0 +1,25 @@
+import subprocess
+import time
+
+import pymysql as pymysql
+
+USERDB = 'root'
+PASSDB = '1234'
+DB = 'anb'
+
+def main():
+    con = pymysql.connect(host="localhost", user=USERDB, passwd=PASSDB, db=DB, charset='utf8',
+                          init_command='SET NAMES UTF8', cursorclass=pymysql.cursors.DictCursor, autocommit=True)
+    cur = con.cursor()
+    cur.execute(
+            """SELECT id FROM users WHERE is_admin = 0""")
+    res = cur.fetchall()
+    cur.close()
+    con.close()
+    for r in res:
+        subprocess.Popen(['java', '-jar', './parser/anb-1.0-jar-with-dependencies.jar', 'anb', r['id']])
+        time.sleep(15*60)
+
+
+if __name__ == "__main__":
+    main()
